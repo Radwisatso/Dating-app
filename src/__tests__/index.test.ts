f74eab1ad0503bc3189ed1e26fdeb686c0d83b7b
Raditya Dwisatrio Santoso <dwisatrioraditya@gmail.com>
2024-12-10 18:01:09 +0700

    feat: adding Premium Feature test

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -10,6 +10,7 @@ beforeAll(() => {
 });
 
 let token: string;
+let userId: string;
 describe("Introduction", () => {
   test("Should've give response: Hello Dating App!", async () => {
     const response = await app.request("/");
@@ -33,7 +34,9 @@ describe("Creating new user", () => {
       method: "POST",
       body: JSON.stringify(body),
     });
+    const result = JSON.parse(await response.text());
     expect(response.status).toBe(201);
+    userId = result.id;
   });
 
   test("POST - Wrong email format 400", async () => {
@@ -134,11 +137,14 @@ describe("Swipes feature", () => {
   });
 
   test("POST - Can't meet the same user twice", async () => {
-    const users = await prisma.user.findMany();
-    const selectedUser = users[0];
+    const user = await prisma.user.findUnique({
+        where: {
+            email: "testuser@example.com"
+        }
+    });
 
     const body = {
-      swiped_user_id: selectedUser.id,
+      swiped_user_id: user!.id,
       swipe_type: "LIKE",
     };
 
@@ -155,5 +161,69 @@ describe("Swipes feature", () => {
       "error",
       expect.stringContaining("You have already swiped on this user")
     );
+  });
+});
+
+describe("Premium Feature", () => {
+  test("POST - User can buy premium subscription", async () => {
+    const premiumPackage = await prisma.premiumPackage.findFirst({
+      where: {
+        name: "Gold",
+      },
+    });
+    const body = {
+      premium_package_id: premiumPackage!.id,
+    };
+
+    const response = await app.request("/auth/subscriptions", {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+
+    expect(response.status).toBe(201);
+  });
+
+  test("POST - User has unlimited swipe quota", async () => {
+    await prisma.dailyLimit.update({
+      data: {
+        swipe_count: 10,
+      },
+      where: {
+        user_id: userId,
+      },
+    });
+
+    const dummy = {
+        email: "anothertesting@mail.com",
+        password: "12345678",
+        name: "Test user",
+        gender: "female",
+        date_of_birth: "1999/01/01",
+        is_premium: false,
+        verified: false,
+      };
+
+      const dummyResponse = await app.request("/users", {
+        method: "POST",
+        body: JSON.stringify(dummy),
+      });
+
+      const body = {
+        swiped_user_id: JSON.parse(await dummyResponse.text()).id,
+        swipe_type: "LIKE",
+      };
+
+    const response = await app.request("/auth/swipes", {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+
+    expect(response.status).toBe(200)
   });
 });
